8810d3710597c6639f8933967e9691edf16c9cd6
Gareth Dwyer <sixhobbits@users.noreply.github.com>
2022-04-26 12:03:55 +0200

    refactor and black format

--- app/make_squares.py
+++ app/make_squares.py
@@ -1,8 +1,11 @@
-from PIL import Image, ImageDraw, ImageFont
 import base64
-import random
 import json
+import random
 import io
+
+from PIL import Image, ImageDraw
+
+BLACK = (0, 0, 0, 255)
 
 palettes = []
 with open("palettes.json") as f:
@@ -11,37 +14,37 @@ with open("palettes.json") as f:
         palette = [tuple(x) for x in p]
         palettes.append(palette)
 
-BLACK = (0, 0, 0, 255)
 
-def create():
+def create(save_path):
     img = Image.new("RGBA", (1000, 1000), (255, 255, 255, 255))
     d = ImageDraw.Draw(img)
 
-
     num_recs = random.randrange(20, 40)
-    min_size = random.randrange(20,50)
-    max_size = min_size + random.randrange(20,100)
+    min_size = random.randrange(20, 50)
+    max_size = min_size + random.randrange(20, 100)
     colors = random.choice(palettes)
-    stroke_width = random.randrange(1,5)
+    stroke_width = random.randrange(1, 5)
     xoffset = 0
     yoffset = 0
     for row in range(num_recs):
         for rec in range(num_recs):
             topleftx = random.randrange(0, 50) + xoffset
             toplefty = random.randrange(0, 50) + yoffset
-            bottomrightx = topleftx + random.randrange(min_size,max_size)
-            bottomrighty = toplefty + random.randrange(min_size,max_size)
+            bottomrightx = topleftx + random.randrange(min_size, max_size)
+            bottomrighty = toplefty + random.randrange(min_size, max_size)
             color = random.choice(colors)
             xoffset += 50
-            r = d.rectangle([topleftx, toplefty, bottomrightx, bottomrighty], fill=color, outline=BLACK, width=stroke_width)
+            r = d.rectangle(
+                [topleftx, toplefty, bottomrightx, bottomrighty],
+                fill=color,
+                outline=BLACK,
+                width=stroke_width,
+            )
         yoffset += 50
         xoffset = 0
-    #img.save(outname)
+    img.save(save_path)
     image = io.BytesIO()
     img.save(image, "PNG")
     image.seek(0)
     img_b64 = base64.b64encode(image.getvalue()).decode()
     return img_b64
-    
-#for i in range(10):
-#    create(f"image{i}.png")
--- app/views.py
+++ app/views.py
@@ -5,39 +5,29 @@ import io, base64
 from PIL import Image
 import os
 
-db_directory = os.getenv('PERSISTENT_STORAGE_DIR', "/tmp")
+tmp_file_path = "/tmp/imgnew.png"
+
 
 @app.route("/", methods=["GET"])
 def index():
-    graphic_image = create()
-    img = Image.open(io.BytesIO(base64.decodebytes(bytes(graphic_image, "utf-8"))))
-    img.save(os.path.join(db_directory, "imgnew.png"))
-    return render_template('home.html', image=graphic_image)
+    graphic_image = create(tmp_file_path)
+    return render_template("home.html", image=graphic_image)
+
 
 @app.route("/generate-another", methods=["GET"])
 def generate_another():
-    graphic_image = create()
-    img = Image.open(io.BytesIO(base64.decodebytes(bytes(graphic_image, "utf-8"))))
-    img.save(os.path.join(db_directory, "imgnew.png"))
+    graphic_image = create(tmp_file_path)
     response = f"""
     <img id="new-image" src="data:image/png;base64,{graphic_image}" />
     """
     return response
 
-#Straight away download with no intermediary save() step
+
 @app.route("/download", methods=["GET", "POST"])
 def download():
-    #if request.method == 'POST':
-    #base64_str = request.form['image']
-    #image = io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8")))
-    return send_file(os.path.join(db_directory, "imgnew.png"), mimetype='image/png', as_attachment=True, download_name="graphic.png")
-
-#First save the image using the Pillow package
-@app.route("/download-with-save", methods=["GET", "POST"])
-def download_with_save():
-    if request.method == 'POST':
-        base64_str = request.form['image']
-        img = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
-        img.save('imgnew.png')
-        return send_file("../imgnew.png", mimetype='image/png', as_attachment=True, download_name="graphic.png")
-    
+    return send_file(
+        tmp_file_path,
+        mimetype="image/png",
+        as_attachment=True,
+        download_name="graphic.png",
+    )
