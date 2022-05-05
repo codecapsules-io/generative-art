e943b0c4e95f674ed6a2f1bd2b6bd93c57d27bc1
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-05-05 15:58:59 +0200

    Update response of generate_another()

--- app/views.py
+++ app/views.py
@@ -20,4 +20,44 @@ def generate_another():
     response = f"""
     <img id="new-image" src="data:image/png;base64,{graphic_image}" />
     """
+    return responsefrom app import app
+from flask import render_template, send_file, request
+from app.make_squares import create
+import io, base64
+from PIL import Image
+import os
+
+tmp_file_path = "/tmp/imgnew.png"
+
+
+@app.route("/", methods=["GET"])
+def index():
+    graphic_image = create(tmp_file_path)
+    return render_template("home.html", image=graphic_image)
+
+
+@app.route("/generate-another", methods=["GET"])
+def generate_another():
+    graphic_image = create(tmp_file_path)
+    response = f"""
+    <div hx-target="this" hx-swap="outerHTML">
+  {% if image %}
+  <div class="image-frame">
+      <img id="new-image" src="data:image/png;base64,{graphic_image}" />
+  </div>
+  {% endif %}
+
+  <div class="button-row">
+  {% if image %}
+      <a download="art.png" href="data:image/png;base64,{graphic_image}">
+      <button class="btn btn-primary">Download</button></a>
+  </form>
+  {% endif %}
+  <button class="btn btn-primary" hx-get="/generate-another">
+      I hate this art, make me another
+  </button>
+  </div>
+</div>
+    """
     return response
+
